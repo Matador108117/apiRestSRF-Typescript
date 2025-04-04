40388c392a3c22a91e12389c7820aeaa7796876f
matador108117 <zS23004741@estudiantes.uv.mx>
2025-04-04 14:44:42 -0600

    initial dto salida

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import router from '../routes/Router.js';
 import { sequelize } from '../database/config/database.js';
-// rama develop
+// rama dto entrada
 dotenv.config();
 
 const app = express();
