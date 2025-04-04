2f22dbbcf909d01b162cd23aea1790bb09655875
matador108117 <zS23004741@estudiantes.uv.mx>
2025-04-04 14:31:36 -0600

    initial release

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import router from '../routes/Router.js';
 import { sequelize } from '../database/config/database.js';
-
+// rama release
 dotenv.config();
 
 const app = express();
