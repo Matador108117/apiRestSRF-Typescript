c6ed727ca9b55ff7192c4a20e8714e41911952ed
matador108117 <zS23004741@estudiantes.uv.mx>
2025-05-31 17:15:24 -0600

    clean middleware

--- src/middlewares/authenticateToken.ts
+++ src/middlewares/authenticateToken.ts
@@ -6,7 +6,7 @@ const SECRET = process.env.JWT_SECRET || 'super_secreto_123';
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization'];
-    const token = authHeader?.split(' ')[1]; // Formato: Bearer <token>
+    const token = authHeader?.split(' ')[1];
     if (!token) return res.status(401).json({ error: errorMessages.ERROR_401_NOT_TOKEN });
     jwt.verify(token, SECRET, (err, user) => {
         if (err) return res.status(403).json({ error: errorMessages.ERROR_403_INVALID_TOKEN, details: err });
